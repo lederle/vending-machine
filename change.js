2d38271b0baec5f86bb29de26340e495bc6a65de
mike lederle <mrlederle@gmail.com>
2021-01-29 04:08:31 -0500

    add jsdoc example

--- change.js
+++ change.js
@@ -3,6 +3,15 @@ if (typeof module !== 'undefined' && module.exports) {
   module.exports = getChange;  // allows CommonJS/Node.js require()
 }
 
+/**
+ * getChange accepts two parameters (totalPayable and cashPaid) and calculates
+ * the change in "coins" that needs to be returned.
+ * @param {number} totalPayable the integer amount (in pennies) to be paid
+ * @param {number} cashPaid the integer amount (in pennies) the person paid
+ * @returns {array} list of coins we need to dispense to the person as change
+ * @example
+ * getChange(215, 300); // returns [50, 20, 10, 5]
+ */
 function getChange(totalPayable, cashPaid) {
   'use strict';
   var change = [];
